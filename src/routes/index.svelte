4980ab8ed2bcca46a7ffb2f0109580098d6ca6a1
takoyaro <thetakoyaro@gmail.com>
2021-09-30 05:03:00 +0900

    feat: 🎸 boilerplate
    First commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import Logo from '$lib/Logo/Logo.svelte';
+</script>
+
+<main>
+    <div class="flex flex-row content-evenly justify-evenly">
+        <div class="flex flex-col w-1/2">
+            <h1>SvelteKit</h1>
+            <a href="https://kit.svelte.dev">Documentation</a>
+        </div>
+        <div class="flex flex-col w-1/2">
+            <h1>Tailwind</h1>
+            <a href="tailwindcss.com/docs/">Documentation</a>
+        </div>
+    </div>
+    <div class="flex flex-row content-evenly justify-evenly">
+        <div class="flex flex-col w-1/2 text-center items-center">
+            <Logo name="svelte"/>
+        </div>
+        <div class="flex flex-col w-1/2 items-center justify-center">
+            <Logo name="tailwind"/>
+        </div>
+    </div>
+</main>
+
+<style lang="postcss">
+    main{
+        @apply flex flex-col h-full justify-center;
+    }
+    h1{
+        @apply text-4xl text-center;
+    }
+    a{
+        @apply text-center p-4;
+    }
+</style>
---
+++ svelte.config.js
@@ -0,0 +1,17 @@
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [preprocess({
+        postcss: true
+    })],
+
+	kit: {
+		// hydrate the <body> element in src/app.html
+		target: 'body'
+	}
+};
+
+export default config;
